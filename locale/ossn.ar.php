39a68b094b49642540c18738259ba95c23bfe9d9
Eyad Tamer <eyadtamer2121@gmail.com>
2021-11-05 19:09:22 +0200

    Create Translation file for Arabic (ossn.ar.php)
    Added translation for Arabic

---
+++ locale/ossn.ar.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+
+ * Open Source Social Network
+
+ *
+
+ * @package Open Source Social Network
+
+ * @author    OSSN Core Team <info@openteknik.com>
+
+ * @copyright (C) OPENTEKNIK  LLC, COMMERCIAL LICENSE
+
+ * @license   OPENTEKNIK  LLC, COMMERCIAL LICENSE, COMMERCIAL LICENSE https://www.openteknik.com/license/commercial-license-v1
+
+ * @link      http://www.opensource-socialnetwork.org/licence
+
+ */
+
+$ar = array(
+
+		'singupprofilephoto' => 'اختر صورة الملف الشخصي (أكبر حجم : %s MB)',
+
+		'singupprofilephoto:error:matching' => '.الصورة التي أضفتها غير صالحة. من فضلك قم بالتحقق من الملف الذي اضافته.',
+
+);
+
+ossn_register_languages('ar', $ar);
