3fc359bd7a1c45366d91038c233c74e747eaccc9
Rafael Amorim <3620428+rafaelmamorim@users.noreply.github.com>
2023-04-10 16:52:01 -0300

    Create ossn.pt.php

---
+++ locale/ossn.pt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package Open Source Social Network
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OPENTEKNIK  LLC, COMMERCIAL LICENSE
+ * @license   OPENTEKNIK  LLC, COMMERCIAL LICENSE, COMMERCIAL LICENSE https://www.openteknik.com/license/commercial-license-v1
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$pt = array(
+		'singupprofilephoto' => 'Imagem de perfil (Tam. Máx : %s MB)',
+		'singupprofilephoto:error:matching' => 'A imagem do perfil é inválida. Use somente arquivos de imagem.',
+);
+ossn_register_languages('pt', $pt); 
