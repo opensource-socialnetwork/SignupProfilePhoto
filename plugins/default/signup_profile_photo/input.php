bbdb82b50b1fa3c4d23851a41a8f07f3143fcebf
Rafael Amorim <3620428+rafaelmamorim@users.noreply.github.com>
2023-04-10 17:06:31 -0300

    Update input.php
    Added parameter on input to accept images only

--- plugins/default/signup_profile_photo/input.php
+++ plugins/default/signup_profile_photo/input.php
@@ -6,7 +6,7 @@
 ?>
 <script>
 	$(document).ready(function(){
-			var image = '<div> <label><?php echo ossn_print('singupprofilephoto', array($memory_limit));?></label> <input type="file" name="profilephoto" class="form-control" /> <div class="margin-top-10"></div> </div>';		
+			var image = '<div> <label><?php echo ossn_print('singupprofilephoto', array($memory_limit));?></label> <input type="file" name="profilephoto" class="form-control" accept="image/*"/> <div class="margin-top-10"></div> </div>';		
 			$(image).insertBefore('#ossn-home-signup #ossn-signup-errors');
 	});
 </script>
